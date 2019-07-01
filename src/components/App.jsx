d41a37440362d69ec07421530391f4bc63083577
Student <example@example.com>
2019-07-01 13:38:12 -0700

    create react project

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+function Apples(){
+  return (
+    <div>
+      <Header/>
+      <TicketList/>
+    </div>
+  );
+}
+
+export default Apples;

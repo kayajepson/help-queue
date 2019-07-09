323f9eb7cb7f0d162f9182d42aae6a0e77a78976
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-07-09 03:40:37 -0700

    add new components and introduce state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
-import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
+import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
 
 import MySampleComponent from './ImageLoaderComponentExample';
 
-function App(){
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
-        <Route component={Error404} />
-      </Switch>
-      <MySampleComponent/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+  }
+  
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={TicketList} />
+          <Route path='/newticket' component={NewTicketControl} />
+          <Route component={Error404} />
+        </Switch>
+        <MySampleComponent/>
+      </div>
+    );
+  }
 }
+
 
 export default App;

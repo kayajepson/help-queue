80331c147ed8864d59a8992302dd5c8975ba4fd8
Kaya Jepson and Stuart McKay <kaya+stuart@>
2019-07-16 11:56:04 -0700

    implement Redux

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
 import Moment from 'moment';
+import { connect } from 'react-redux';
 
 function NewTicketForm(props){
   let _names = null;
@@ -9,7 +10,17 @@ function NewTicketForm(props){
   let _issue = null;
 
   function handleNewTicketFormSubmission(event) {
+    const { dispatch } = props;
     event.preventDefault();
+    const action = {
+      type: 'ADD_TICKET',
+      id: null,
+      names: _names.value,
+      location: _location.value,
+      issue: _issue.value,
+      timeOpen: new Moment()
+    };
+    dispatch(action);
     // this calls handleAddingNewTicketToList() from App to add the new ticket to the master list
     props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4(), timeOpen: new Moment()});
     _names.value = '';
@@ -44,4 +55,8 @@ NewTicketForm.propTypes = {
   onNewTicketCreation: PropTypes.func
 };
 
-export default NewTicketForm;
+// These 2 lines are the same as the one below
+// NewTicketForm = connect()(NewTicketForm);
+// export default NewTicketForm;
+
+export default connect()(NewTicketForm);
